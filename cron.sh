b59d3ca409a37e0fa56f487484b2206b98386efe
Yoav Arbiv <yoavarbiv@Mac.attlocal.net>
2026-05-04 17:54:59 -0700

    Try cleaning up cron

--- cron.sh
+++ cron.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 set -e
+
+LOGFILE="/var/log/bandcamp-downloader.log"
+
+log() {
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] $*" >> "$LOGFILE"
+}
+
+log "=== Starting bandcamp sync ==="
+
 /root/bandcamp-downloader/.venv/bin/python /root/bandcamp-downloader/bandcamp-downloader.py \
     --cookies /root/bandcamp-downloader/cookies/cookies.txt \
     --directory /root/bandcamp-downloader/music/downloads \
@@ -7,7 +16,10 @@ set -e
     -v \
     --extract \
     --ntfy-topic slA41wTTTJj924eb \
-    yarbiv
+    yarbiv >> "$LOGFILE" 2>&1
 
-# We handle the -m /root/music bit in configs.
-/root/bandcamp-downloader/.venv/bin/beet import -q /root/bandcamp-downloader/music/downloads
+log "=== Starting beet import ==="
+
+/root/bandcamp-downloader/.venv/bin/beet import -q /root/bandcamp-downloader/music/downloads >> "$LOGFILE" 2>&1
+
+log "=== Done ==="
